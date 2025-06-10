60ed286b615d96a84d351d7edf6e1afc70c78c1e
aleksandrostanin <ostanin.sanek@gmail.com>
2025-06-10 14:30:16 +0400

    fix settings

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 from pydantic_settings import BaseSettings
-from pydantic import ConfigDict, field_validator
-from typing import Optional
+from pydantic import ConfigDict
 
 
 class Settings(BaseSettings):
@@ -11,13 +10,7 @@ class Settings(BaseSettings):
     database_username: str
     secret_key: str
     algorithm: str
-    access_token_expire_minutes: Optional[int]
-
-    @field_validator('access_token_expire_minutes', mode='before')
-    def parse_expire_minutes(cls, v):
-        if isinstance(v, str) and v.strip() in ['***', '']:
-            return 30
-        return int(v)
+    access_token_expire_minutes: int
 
     model_config = ConfigDict(env_file=".env")
 
